98cd61d77a2abd6d8709faf7311d9e9ea38dfd72
ryo-h <ryo-h@weseek.co.jp>
2023-12-05 22:29:13 +0900

    isr - top page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,31 @@
 import React from 'react';
+import { GetStaticProps, NextPage } from 'next';
+import Link from 'next/link';
 
-const Top = () => {
-  return <span>hello world</span>;
+type TopProps = {
+  message: string;
+};
+
+const Top: NextPage<TopProps> = (props) => {
+  return (
+    <>
+      <Link href="/ssr">Go to SSR</Link>
+      <span>hello world</span>
+      <p>{props.message}</p>
+    </>
+  );
+};
+
+export const getStaticProps: GetStaticProps = async (constext) => {
+  const timestamp = new Date().toLocaleDateString();
+  const message = `${timestamp} に getStaticProps 実行された`;
+  console.log(message);
+  return {
+    props: {
+      message,
+    },
+    revalidate: 30,
+  };
 };
 
 export default Top;
