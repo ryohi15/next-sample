ff0380a096738f116068377e8c681ee17114fc64
ryo-h <ryo-h@weseek.co.jp>
2023-12-06 22:22:01 +0900

    4.5 styled component intro

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,6 +2,13 @@ import React from 'react';
 import { GetStaticProps, NextPage } from 'next';
 import Link from 'next/link';
 
+import styles from '../src/app/page.module.css';
+import styled from 'styled-components';
+
+const H1 = styled.h1`
+  color: red;
+`;
+
 type TopProps = {
   message: string;
 };
@@ -9,9 +16,15 @@ type TopProps = {
 const Top: NextPage<TopProps> = (props) => {
   return (
     <>
-      <Link href="/ssr">Go to SSR</Link>
-      <span>hello world</span>
-      <p>{props.message}</p>
+      <div className={styles.container}>
+        <main className={styles.main}>
+          <H1>
+            Welcome to <a href="https://nextjs.org">Next.js!1</a>
+          </H1>
+          <Link href="/ssr">Go to SSR</Link>
+          <p>{props.message}</p>
+        </main>
+      </div>
     </>
   );
 };
